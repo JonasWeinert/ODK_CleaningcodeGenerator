9173903d86b284b892157b9bfb434902948adbc1
JonasWeinert <99423051+JonasWeinert@users.noreply.github.com>
2023-03-28 19:35:31 +0200

    changed favicon

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title='ODKCleaner')
+st.set_page_config(page_title='ODKCleaner', page_icon="🧡")
 
 
 def load_css(file_name):
